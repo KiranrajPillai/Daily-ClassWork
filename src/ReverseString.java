41e2167f88a373e06ad36ed14c7375be6d4aec52
KiranrajPillai <kiranrajpillai1997@gmail.com>
2021-10-21 13:18:48 +0530

    To Reverse a String.

---
+++ src/ReverseString.java
@@ -0,0 +1,11 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String string = "Kiran";
+        String reverseString = "";
+        for(int i = string.length()-1; i >= 0; i--){
+            reverseString = reverseString + string.charAt(i);
+        }
+        System.out.println("Original string: " + string);
+        System.out.println("Reverse of given string: " + reverseString);
+    }
+}
